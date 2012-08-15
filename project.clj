9d72e05741120bdfae5e4f8011fd083f0ce078b2
Christopher Dwyer <foulplay2@gmail.com>
2012-08-15 19:16:47 -0400

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject DrugDolls "1.0.0-SNAPSHOT"
+  :description "Drug Dolls problem for Atomic Object Application"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/DrugDolls/test/core.clj
@@ -0,0 +1,6 @@
+(ns DrugDolls.test.core
+  (:use [DrugDolls.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
